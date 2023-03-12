8b48b07cffbc2b8dde21924d1f24fefed0e9b04e
MisuykMaryna <misyuk.marina@gmail.com>
2023-03-12 17:29:34 +0100

    hm5

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './css/styles.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import Notiflix from 'notiflix';
+import axios from 'axios';
